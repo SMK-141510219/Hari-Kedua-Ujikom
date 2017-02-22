37b4155c46c4729df782993b5420f3d9a8715b17
smk-141510219 <141510219@smkassalaambandung.sch.id>
2017-02-22 16:57:49 +0700

    Hari Kedua Ujikom

---
+++ resources/views/jabatan/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content')
+<h1>Tambah Data</h1>
+{!!Form::open(['url'=>'jabatan'])!!}
+<div class="form-group">
+	{!!Form::label('kode','Kode jabatan:')!!}
+	{!!Form::text('kode_jabatan',null,['class'=>'form-control'])!!}
+</div>
+<div class="form-group">
+{!!Form::label('nama','Nama Jabatan :')!!}
+{!!Form::text('nama_jabatan',null,['class'=>'form-control'])!!}	
+</div>
+<div class="form-group">
+{!!Form::label('besaran uang','Besaran Uang :')!!}
+{!!Form::text('besaran_uang',null,['class'=>'form-control'])!!}	
+</div>
+
+<div class="form-group">
+{!!Form::submit('Save',['class'=>'btn btn-primary form-control'])!!}
+
+	
+</div>
+{!!Form::close()!!}
+@stop
---
+++ resources/views/jabatan/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<h1>Update</h1>
+{!!Form::model($jabatan,['method'=>'PATCH','route'=>['jabatan.update',$jabatan->id]])!!}
+<div class="form-group">
+	{!!Form::label('kode','Kode jabatan:')!!}
+	{!!Form::text('kode_jabatan',null,['class'=>'form-control'])!!}
+</div>
+
+<div class="form-group">
+{!!Form::label('nama','Nama Jabatan :')!!}
+{!!Form::text('nama_jabatan',null,['class'=>'form-control'])!!}	
+<div class="form-group">
+{!!Form::label('besaran','Besaran Uang :')!!}
+{!!Form::text('besaran_uang',null,['class'=>'form-control'])!!}	
+</div>
+</div>
+<div class="form-group">
+{!!Form::submit('Update',['class'=>'btn btn-primary'])!!}	
+</div>
+{!!Form::close()!!}
+@stop
